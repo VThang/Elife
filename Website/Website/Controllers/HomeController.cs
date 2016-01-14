e9021a1572ad3b8b39c55b4ec25dfbb1e1976640
VThang <leejaewoon9306@gmail.com>
2016-01-14 08:54:47 +0700

    error

--- Website/Website/Controllers/HomeController.cs
+++ Website/Website/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Website.Models;
 
 namespace Website.Controllers
 {
     public class HomeController : Controller
     {
+        private WebsiteDatabaseEntities DB = new WebsiteDatabaseEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -29,6 +32,10 @@ namespace Website.Controllers
         }
 
         public ActionResult Project()
+        {
+            return View();
+        }
+        public ActionResult Test()
         {
             return View();
         }
